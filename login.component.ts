49248460c411708fbd4bbf66f923ddb15781d1ac
Quezia Ribeiro <100374062+queziards@users.noreply.github.com>
2025-07-24 18:30:32 -0300

    Login - Web

---
+++ login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Router } from 'express';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [RouterModule,],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
